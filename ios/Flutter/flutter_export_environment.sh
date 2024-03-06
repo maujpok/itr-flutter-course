c2db0ff8561bd5e77128a3f8d2a0ba4fb99e5f94
Mauricio <maujpok@gmail.com>
2024-03-06 15:18:43 -0300

    correccion

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/maujpok/flutter/versions/3.13.0"
-export "FLUTTER_APPLICATION_PATH=/Users/maujpok/Development/itr/itr-flutter-course"
+export "FLUTTER_APPLICATION_PATH=/Users/maujpok/Development/itr/itr_course_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
