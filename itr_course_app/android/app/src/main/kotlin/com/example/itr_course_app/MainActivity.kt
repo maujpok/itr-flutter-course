394c08ef24e7cb80084d7ad2bde148ad37b2f7a1
Mauricio <maujpok@gmail.com>
2024-03-06 10:01:01 -0300

    ajustes

--- itr_course_app/android/app/src/main/kotlin/com/example/itr_course_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.itr_course_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- itr_course_app/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:itr_course_app/app/app.dart';
-
-void main() {
-  runApp(const App());
-}
