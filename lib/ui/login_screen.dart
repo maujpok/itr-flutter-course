3c1c09ba8161aedea5047d0220216dfa07682d10
LuCarrillo <Luis_Carrillo@itrsa.com.ar>
2024-03-09 16:54:48 -0300

    Tested on Android

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -10,29 +10,169 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
+    return const Scaffold(
+      appBar: CustomAppBar(),
       body: Column(
         children: [
-          Image.network('https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png'),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Email'),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Column(
+              children: [
+                LogoImage(),
+                EmailInput(),
+                SizedBox(height: 20),
+                PasswordInput(),
+                ForgotPasswordButton(),
+                LoginButton(),
+              ],
+            ),
           ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Password'),
+          Flexible(
+            child: BottomBar(),
           ),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Forgot Password'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Login'),
-          ),
-          const Text('New User? Create Account')
         ],
+      ),
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: const Color(0xFF00BFFF),
+      title: const Text(
+        'Login Page',
+        style: TextStyle(color: Colors.white)
+      ),
+    );
+  }
+}
+
+class LogoImage extends StatelessWidget {
+  const LogoImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 70, bottom: 20),
+      child: Container(
+        alignment: Alignment.center,
+        child: SizedBox(
+          height: 100,
+          width: 250,
+          child: Image.network('https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png'),
+        ),
+      ),
+    );
+  }
+}
+
+class EmailInput extends StatelessWidget {
+  const EmailInput({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: const BorderSide(color: Colors.grey),
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordInput extends StatelessWidget {
+  const PasswordInput({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: const BorderSide(color: Colors.grey),
+        ),
+      ),
+    );
+  }
+}
+
+class ForgotPasswordButton extends StatelessWidget {
+  const ForgotPasswordButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      child: const Text(
+        'Forgot Password',
+        style: TextStyle(color: Color(0xFF00BFFF)),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 44,
+      width: 220,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF00BFFF)),
+        ),
+        child: const Text(
+        'Login',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        )
+        ),
+      ),
+    );
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        margin: const EdgeInsets.only(top: 0, bottom: 10),
+        child: RichText(
+          text: TextSpan(
+            text: 'New User? ',
+            style: const TextStyle(color: Colors.black),
+            children: <TextSpan>[
+              TextSpan(
+                text: 'Create Account',
+                style: const TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    print('se hizo tab en Create Account');
+                  },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
